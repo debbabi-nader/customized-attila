b8bed92bd7a5ca0a7cd6ae984a37ab4ecb61e81c
Nader Debbabi <nader.debbabi.30@gmail.com>
2019-09-10 23:49:09 +0100

    Changed headers and menu styles

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -63,6 +63,22 @@ jQuery(function($) {
 	});
 
 	/* ==========================================================================
+	   Smooth Page Scroll
+	   ========================================================================== */
+
+	$("#scroll-cue").on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
+
+	/* ==========================================================================
 	   Reading Progress
 	   ========================================================================== */
 
